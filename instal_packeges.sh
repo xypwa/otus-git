899a30a4c009928cdc44928884eaed0901669dac
Евгений П <sasisachko@gmail.com>
2024-05-22 09:52:01 +0800

    Update instal_packeges.sh

--- instal_packeges.sh
+++ instal_packeges.sh
@@ -8,9 +8,4 @@ apt-get install tree -y;
 apt-get install mc -y;
 apt-get install rsync -y;
 apt install sshpass;
-
-
-
-
-
-
+apt-get install -y php7.4 php7.4-cli php7.4-fpm php7.4-json php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php-pear php7.4-bcmath
