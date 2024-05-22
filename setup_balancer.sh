6ac1cf3a6521285dec9b7e2f114ae0bbde63e0d0
Евгений П <sasisachko@gmail.com>
2024-05-22 09:34:19 +0800

    Update setup_balancer.sh

--- setup_balancer.sh
+++ setup_balancer.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
-ip_app_main=192.168.71.133;
-#ip_app_rsrv=192.168.71.133;
+# Пропускаем проверку незнакомых хостов при ssh подключении #
+echo "    UserKnownHostsFile /dev/null\n    StrictHostKeyChecking no" >> /etc/ssh/ssh_config;
+
+ip_app_node-1=192.168.71.140;
+#ip_app_node-2=192.168.71.143;
 #ip_db_master=192.168.71.133;
 #ip_db_slave=192.168.71.133;
 #ip_elk=192.168.71.133;
 
+
+
 # создание сертификата
-mkdir ~/certs && cd ~/certs;
+#mkdir ~/certs && cd ~/certs;
 
 #openssl genrsa -out localhost_rootCA.key 2048;
 #openssl req -newkey rsa:2048 -nodes -keyout localhost_rootCA.key -out localhost_rootCA.csr < cert_pass_params.txt
 #openssl x509 -signkey localhost_rootCA.key -in localhost_rootCA.csr -req -days 365 -out localhost_rootCA.crt;
-openssl req -newkey rsa:2048 -nodes -keyout localhost_rootCA.key -x509 -days 365 -out localhost_rootCA.crt < cert_pass_params.txt
+#openssl req -newkey rsa:2048 -nodes -keyout localhost_rootCA.key -x509 -days 365 -out localhost_rootCA.crt < cert_pass_params.txt
 #где pass.txt содержит значения для read команд программы
 
 
 #
 # настройка авторизации по ключу
 #
-cd ~/.ssh;
+cd ~;
 echo 'qwertyzxv' > pass.txt
-## для app-main
-ssh-keygen -t rsa -f app_main -N ''
-ssh-keygen -t rsa -f app_rsrv -N ''
-ssh-keygen -t rsa -f db_master -N ''
-ssh-keygen -t rsa -f db_slave -N ''
-ssh-keygen -t rsa -f elk -N ''
 
-#
+# сгенерируем общий ключ для всех хостов
+ssh-keygen -t rsa -f general -N ''
+
+# закинем список хостов в файл
+#echo "$ip_app_node-1 $ip_app_node-2 $ip_db_master $ip_db_slave $ip_elk" > ~/my_hosts.txt
+echo "$ip_app_node-1" > my_hosts.txt
+
 # закидываем ключи на узлы
-#
-sshpass -f pass.txt ssh-copy-id -i app_main xypwa@$ip_app_main;
+for ip in `cat ~/my_hosts.txt`; do
+    sshpass -f ~/pass.txt ssh-copy-id -i ~/.ssh/general.pub $ip
+done
+
+#sshpass -f pass.txt ssh-copy-id -i app_main xypwa@$ip_app_main;
 #sshpass -f pass.txt ssh-copy-id -i app_rsrv xypwa@$ip_app_rsrv;
 #sshpass -f pass.txt ssh-copy-id -i db_master xypwa@$ip_db_master;
 #sshpass -f pass.txt ssh-copy-id -i db_slave xypwa@$ip_db_slave;
