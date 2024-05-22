57bb2d89966cfd7e6b725439e851e69c63bb8223
Евгений П <sasisachko@gmail.com>
2024-05-22 09:35:44 +0800

    Update instal_packeges.sh

--- instal_packeges.sh
+++ instal_packeges.sh
@@ -1,15 +1,12 @@
 #!/bin/bash/
 
-cd ~
 # установка пакетов
 
 apt update;
 apt-get install nginx -y;
-apt-get install -y php7.4 php7.4-cli php7.4-fpm php7.4-json php7.4-pdo php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php-pear php7.4-bcmath
 apt-get install tree -y;
 apt-get install mc -y;
 apt-get install rsync -y;
-#apt install iptables-persistent;
 apt install sshpass;
 
 
