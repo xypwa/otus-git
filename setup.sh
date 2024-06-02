8bd86224d0e541ac1a415fe8e81379dd802e133d
Евгений П <sasisachko@gmail.com>
2024-06-02 23:57:21 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+APP_NODE_1='192.168.71.140';
+#APP_NODE_2='192.168.71.140';
+# настройка базы #
+
+mysql -uroot <<EOF
+CREATE DATABASE majordomo DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE USER 'majordomo'@"${APP_NODE_1}" IDENTIFIED WITH 'caching_sha2_password' BY 'qwertyzxv';
+GRANT ALL PRIVILEGES ON majordomo.* TO 'majordomo'@"${APP_NODE_1}" WITH GRANT OPTION;
+GRANT RELOAD, FLUSH_TABLES ON *.* TO 'majordomo'@"${APP_NODE_1}";
+FLUSH PRIVILEGES;
+EOF
+
+sed -i 's/^\(bind-address\s*=\s*\).*$/\10.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf; 
+
+service mysql restart;
