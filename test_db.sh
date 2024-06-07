8a736467629cb0973e1208c49f06c0d470c50f30
Евгений П <sasisachko@gmail.com>
2024-06-07 11:34:11 +0800

    Update test_db.sh

--- test_db.sh
+++ test_db.sh
@@ -16,5 +16,5 @@ branch_db_slave="db_slave";
 echo "Настройка репликации";
 read -p 'Укажите Тип репликации. [1](default) GTID, [2] BINLOG POSITION: ' TYPE;
 echo "$TYPE";
-sshpass -f ~/pass.txt ssh -i ~/.ssh/ganeral xypwa@"$ip_db_master" 'echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}'
-sshpass -f ~/pass.txt ssh -i ~/.ssh/ganeral xypwa@"$ip_db_slave" 'echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}'
+sshpass -f ~/pass.txt ssh -i ~/.ssh/general xypwa@"$ip_db_master" 'echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}'
+sshpass -f ~/pass.txt ssh -i ~/.ssh/general xypwa@"$ip_db_slave" 'echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}'
