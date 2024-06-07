db953f8925d611e091cc13aa131de0d39731b8cc
Евгений П <sasisachko@gmail.com>
2024-06-07 11:28:19 +0800

    Update setup_balancer.sh

--- setup_balancer.sh
+++ setup_balancer.sh
@@ -61,7 +61,7 @@ done < ~/my_hosts.txt
 # настройка репликации БД
 #
 echo "Настройка репликации";
-read -p 'Укажите Тип репликации. [1](default) GTID, [2] BINLOG POSITION' TYPE;
+read -p 'Укажите Тип репликации. [1](default) GTID, [2] BINLOG POSITION: ' TYPE;
 echo "$TYPE";
 sshpass -f ~/pass.txt ssh -i ~/.ssh/ganeral xypwa@"$ip_db_master" 'echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}'
 sshpass -f ~/pass.txt ssh -i ~/.ssh/ganeral xypwa@"$ip_db_slave" 'echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}'
