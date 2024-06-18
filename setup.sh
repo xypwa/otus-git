4e3a826926ea913f95ca0544bb0d9a2b661adcee
Евгений П <sasisachko@gmail.com>
2024-06-18 20:01:16 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,14 +52,19 @@ sed -i "s/Define('DB_USER', '.*');/Define('DB_USER', 'majordomo');/" "$APP_HOME_
 sed -i "s/Define('DB_PASSWORD', '.*');/Define('DB_PASSWORD', 'qwertyzxv');/" "$APP_HOME_DIR/config.php";
 sed -i '/echo "ALL CYCLES STARTED" . PHP_EOL;/a exit(1);' "$APP_HOME_DIR/cycle.php";
 
-echo "${CERTIFICATE_CONFIG}" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
-cp -f /home/xypwa/install/*.pem /var/lib/mysql;
-chown mysql:mysql /var/lib/mysql -R;
+CERTIFICATE_CONFIG="
+ssl_ca=ca.pem
+ssl_cert=server-cert.pem
+ssl_key=server-key.pem
+"
+#echo "${CERTIFICATE_CONFIG}" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
+#cp -f /home/xypwa/install/*.pem /var/lib/mysql;
+#chown mysql:mysql /var/lib/mysql -R;
 service mysql restart;
-SSL_CONFIG_MYSQLI="\$obj = mysqli_init();\nif(file_exists('/home/xypwa/install/ca.pem')) mysqli_ssl_set(\$obj, NULL, NULL, '/home/xypwa/install/ca.pem', NULL, NULL);";
+#SSL_CONFIG_MYSQLI="\$obj = mysqli_init();\nif(file_exists('/home/xypwa/install/ca.pem')) { mysqli_ssl_set(\$obj, NULL, NULL, '/home/xypwa/install/ca.pem', NULL, NULL); }";
 
 #  echo "${BINLOG_POSITION_CONFIG}" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
-sed -i "129a ${SSL_CONFIG_MYSQLI}" "$APP_HOME_DIR/lib/mysqli.class.php"
+#sed -i "129a ${SSL_CONFIG_MYSQLI}" "$APP_HOME_DIR/lib/mysqli.class.php"
 
 chown -R www-data:www-data $APP_HOME_DIR;
 
