024e917c7de7ddf6d1aad7b4a3b50af0a8e0d81e
Евгений П <sasisachko@gmail.com>
2024-06-23 10:49:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,101 +1,67 @@
 #!/bin/bash
 
 TYPE=$1;
-TSL=$2;
-DB=$3;
+DB_MASTER="192.168.71.147";
+DB_NAME='majordomo';
 
-if [ $TYPE -eq 1 ]; then
-    echo "Replication type: GTID"
-else
-    echo "Replication type: BINLOG POSITION"
-fi
-
-echo "TSL: $TSL";
-
-DB_NAME="majordomo";
-APP_NODE_1='192.168.71.140';
-APP_NODE_2='192.168.71.143';
-REPLICA_IP='192.168.71.148';
-systemctl stop mysql;
-
-sed -i 's/^\(bind-address\s*=\s*\).*$/\10.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
-
-GTID_MASTER_CONFIG="
-server-id = 1
+CONF_BINLOG="
+server-id = 2
 log-bin = mysql-bin
-binlog_format = row
+relay-log = mysql-relay-server
+binlog_do_db = majordomo
+read-only = ON
+"
+CONF_GTID="
+server-id = 2
+log-bin = mysql-bin
+relay-log = mysql-relay-server
+binlog_do_db = majordomo
 gtid-mode=ON
 enforce-gtid-consistency
 log-replica-updates
-binlog_do_db = ${DB_NAME}
+read-only = ON
 "
-
-BINLOG_POS_MASTER_CONFIG="
-server-id = 1
-log_bin = mysql-bin
-binlog_format = row
-binlog_do_db = ${DB_NAME}
-log-replica-updates
-"
-CERTIFICATE_CONFIG="
-ssl_ca=ca.pem
-ssl_cert=server-cert.pem
-ssl_key=server-key.pem
-"
-
-if [[ "${TYPE}" -eq '1' ]]; then
-  echo "${GTID_MASTER_CONFIG}" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
-else
-  echo "${BINLOG_POS_MASTER_CONFIG}" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
-fi;
+CERTS="find -type f -name *.pem /home/xypwa/install/ | wc -l"
 
 
-systemctl start mysql;
-
-# настройка базы #
-
-mysql -uroot <<EOF
-CREATE DATABASE majordomo DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-
-CREATE USER 'majordomo'@"${APP_NODE_1}" IDENTIFIED WITH 'caching_sha2_password' BY 'qwertyzxv';
-GRANT ALL PRIVILEGES ON majordomo.* TO 'majordomo'@"${APP_NODE_1}" WITH GRANT OPTION;
-GRANT RELOAD, FLUSH_TABLES ON *.* TO 'majordomo'@"${APP_NODE_1}";
-
-CREATE USER 'majordomo2'@"${APP_NODE_2}" IDENTIFIED WITH 'caching_sha2_password' BY 'qwertyzxv';
-GRANT ALL PRIVILEGES ON majordomo.* TO 'majordomo2'@"${APP_NODE_2}" WITH GRANT OPTION;
-GRANT RELOAD, FLUSH_TABLES ON *.* TO 'majordomo2'@"${APP_NODE_2}";
-
-#CREATE USER 'xypwa@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'qwertyzxv';
-#GRANT ALL PRIVILEGES ON *.* TO 'xypwa'@'%' WITH GRANT OPTION;
-
-CREATE USER 'replicant'@"${REPLICA_IP}" IDENTIFIED WITH 'caching_sha2_password' BY 'qwertyzxv';
-GRANT REPLICATION SLAVE ON *.* to 'replicant'@"${REPLICA_IP}";
-FLUSH PRIVILEGES;
+if [[ "$TYPE" -eq '2' ]]; then
+  echo "$CONF_BINLOG" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
+  FILE=$2; POSITION=$3;
+  mysql -uroot <<EOF
+CREATE DATABASE ${DB_NAME};
+CHANGE REPLICATION SOURCE TO
+SOURCE_HOST = "${DB_MASTER}",
+SOURCE_USER = 'replicant',
+SOURCE_PASSWORD = 'qwertyzxv',
+SOURCE_LOG_FILE="${FILE}",
+SOURCE_LOG_POS=${POSITION},
+GET_SOURCE_PUBLIC_KEY = 1;
 EOF
-
-if [[ "$TSL" = "Y" || "$TSL" = 'y' ]]; then
-  mysql -u root -e "ALTER USER 'replicant'@'${REPLICA_IP}' REQUIRE SSL;"
-  #sed -i "/^bind-address/a\require_secure_transport = ON" /etc/mysql/mysql.conf.d/mysqld.cnf
-  echo "${CERTIFICATE_CONFIG}" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
-  mkdir certs;
-  cp /var/lib/mysql/*.pem ./certs/
-  chown -R xypwa:xypwa ./certs;
+else
+  echo "$CONF_GTID" >> /etc/mysql/mysql.conf.d/mysqld.cnf;
+  mysql -uroot <<EOF
+CREATE DATABASE ${DB_NAME};
+CHANGE REPLICATION SOURCE TO
+SOURCE_HOST = "${DB_MASTER}",
+SOURCE_USER = 'replicant',
+SOURCE_PASSWORD = 'qwertyzxv',
+SOURCE_AUTO_POSITION = 1,
+GET_SOURCE_PUBLIC_KEY = 1;
+EOF
 fi;
 
-if [[ "${TYPE}" -eq "2" ]]; then
-  status=(`mysql -u root -e "SHOW MASTER STATUS;"`);
-  file="${status[5]}";
-  position="${status[6]}";
-  echo "$file" > binlog_file.output; echo "$position" > binlog_pos.output;
-fi;
 
-if [[ "${DB}" -eq "2" && -f /home/xypwa/install/work_dump.sql ]]; then
-  mysql -u root majordomo < /home/xypwa/install/work_dump.sql;
-else 
-  mysql -u root majordomo < /home/xypwa/install/default_dump.sql;
+if [[ "$CERTS" -gt 0 ]]; then
+  cp -f ./*.pem /var/lib/mysql;
+  chown mysql:mysql /var/lib/mysql -R;
+    mysql -uroot <<EOF
+CHANGE MASTER TO SOURCE_SSL_CA = "ca.pem", 
+SOURCE_SSL_CERT = "server-cert.pem", 
+SOURCE_SSL_KEY = "server-key.pem", 
+SOURCE_SSL=1;
+EOF
 fi;
-
-#mysqldump --master-data -u root majordomo > majordomo.sql
-#rsync -avz majordomo.sql xypwa@192.168.71.148:/home/xypwa/
 
 service mysql restart;
+mysql -u root -e "START REPLICA;"
+mysql -u root -e "SHOW REPLICA STATUS;"
