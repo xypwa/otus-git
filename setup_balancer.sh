d4d2b5b28a864c9a43c333ff303eb77221c1084d
Евгений П <sasisachko@gmail.com>
2024-06-26 21:16:34 +0800

    Update setup_balancer.sh

--- setup_balancer.sh
+++ setup_balancer.sh
@@ -80,9 +80,12 @@ if [[ "$TYPE" -eq '2' ]]; then
         # копируем сертификаты mysql с мастера на слейв..
         echo "копируем сертификаты mysql с мастера на слейв ";
         rsync -avz -e "ssh -i ~/.ssh/general" xypwa@"$ip_db_master":/home/xypwa/certs/* ~/tmp/
+        
         rsync -avz -e "ssh -i ~/.ssh/general" ~/tmp/* xypwa@"$ip_db_slave":/home/xypwa/install/
+        rsync -avz -e "ssh -i ~/.ssh/general" ~/tmp/* xypwa@"$ip_app_node_1":/home/xypwa/install/
+        rsync -avz -e "ssh -i ~/.ssh/general" ~/tmp/* xypwa@"$ip_app_node_2":/home/xypwa/install/
     fi;
-    sshpass -f ~/pass.txt ssh -i ~/.ssh/general xypwa@"$ip_db_slave" "echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE} ${FILE} ${POSITION}";
+    sshpass -f ~/pass.txt ssh -i ~/.ssh/general xypwa@"$ip_db_slave" "echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE} ${BKP} ${FILE} ${POSITION}";
 else
     if [[ "$TSL" = 'Y' || "$TSL" = 'y' ]]; then
         mkdir ~/tmp;
@@ -94,7 +97,7 @@ else
         rsync -avz -e "ssh -i ~/.ssh/general" ~/tmp/* xypwa@"$ip_app_node_1":/home/xypwa/install/
         rsync -avz -e "ssh -i ~/.ssh/general" ~/tmp/* xypwa@"$ip_app_node_2":/home/xypwa/install/
     fi;
-    sshpass -f ~/pass.txt ssh -i ~/.ssh/general xypwa@"$ip_db_slave" "echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE}";
+    sshpass -f ~/pass.txt ssh -i ~/.ssh/general xypwa@"$ip_db_slave" "echo qwertyzxv | sudo -S bash /home/xypwa/install/setup.sh ${TYPE} ${BKP}";
 fi;
 
 exit;
