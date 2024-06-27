e8cd90f35fedb6b97301827303f939e0fe52cb2f
Евгений П <sasisachko@gmail.com>
2024-06-28 01:09:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BALANCER_IP="192.168.71.146";
+
 CONF_PATH="/home/xypwa/install/conf.d";
 INCLUDE_NGINX=$1;
 CONF_NGINX_PATH="${CONF_PATH}/logstash-nginx.conf";
@@ -12,15 +14,19 @@ if ! [ -d /etc/logstash/conf.d ]; then
   mkdir /etc/logstash/conf.d;
 fi;
 
+
 if [[ "$INCLUDE_NGINX" = "Y" || "$INCLUDE_NGINX" = "y" ]]; then
-  cp "${CONF_NGINX_PATH} /etc/logstash/conf.d";
+  cp -R "${CONF_NGINX_PATH} /etc/logstash/conf.d";
+  sed -i "s/targets: [\'localhost:9100\']/targets: [\'${BALANCER_IP}:9100\']/" /etc/prometheus/prometheus.yml;
 fi;
 if [[ "$INCLUDE_APACHE" = "Y" || "$INCLUDE_APACHE" = "y" ]]; then
-  cp "${CONF_APACHE_PATH} /etc/logstash/conf.d";
+  cp -R "${CONF_APACHE_PATH} /etc/logstash/conf.d";
 fi;
 if [[ "$INCLUDE_MYSQL" = "Y" || "$INCLUDE_MYSQL" = "y" ]]; then
-  cp "${CONF_MYSQL_PATH} /etc/logstash/conf.d";
+  cp -R "${CONF_MYSQL_PATH} /etc/logstash/conf.d";
 fi;
 
 
 systemctl restart logstash;
+systemctl restart prometheus;
+systemctl restart grafana-server;
